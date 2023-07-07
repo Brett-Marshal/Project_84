64117dadb6b6e01699b8a29145a0ad749b98af27
Brett-Marshal <131263863+Brett-Marshal@users.noreply.github.com>
2023-07-07 18:29:46 +0530

    Update main.js

--- main.js
+++ main.js
@@ -26,17 +26,38 @@ function my_keydown(e)
 {
 	keyPressed = e.keyCode;
 	console.log(keyPressed);
-	
-		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
+
+	if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
 		{
 			alphabetkey()
 			document.getElementById("d1").innerHTML = "you pressed an Alphabet key"
 			console.log("alphabet key")
 		}
-	else{
-		otherkey();
-		document.getElementById("d1").innerHTML="You pressed symbol or other key";
-	}
+		
+		if(keyPressed >=48 && keyPressed<=57)
+		{
+			numberkey()
+			document.getElementById("d1").innerHTML = "you pressed a Nunber key"
+			console.log("number key")
+		}
+		if(keyPressed >=37 && keyPressed<=40)
+		{
+			arrowkey()
+			document.getElementById("d1").innerHTML = "you pressed an Arrow key"
+			console.log("arrow key")
+		}
+		if( keyPressed >= 17 && keyPressed <= 18)
+		{
+			specialkey()
+			document.getElementById("d1").innerHTML = "you pressed a Special key"
+			console.log("special key")
+		}
+		if(( keyPressed >= 1 && keyPressed <= 16) ( keyPressed >= 19 && keyPressed <= 36) ( keyPressed >= 41 && keyPressed <= 47)( keyPressed >= 58 && keyPressed <= 64) ( keyPressed >= 123 && keyPressed <= 222))
+		{
+			otherkey()
+			document.getElementById("d1").innerHTML = "you pressed an Other key"
+			console.log("other key")
+		}
 }
 
 function alphabetkey()
@@ -47,18 +68,21 @@ function alphabetkey()
 }
 function numberkey()
 {
-	
+	img_image = "numkey.png"
+	add()
 }
 function arrowkey()
 {
+	img_image = "Arrkey.png"
+	add()
 }
 function specialkey()
 {
-	
+	img_image = "spkey.png"
+	add()
 }
 function otherkey()
 {
 	img_image="otherkey.png";
 	add();
 }
-	
